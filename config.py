b00330fe4130406fbf1dd59fda2d61f7932c1537
Barry <ibarry@itrust.fr>
2022-08-29 03:28:05 +0200

    fix bdd

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from settings import get_settings
 
-DATABASE_URL = get_settings().database_url
+# DATABASE_URL = get_settings().database_url
+DATABASE_URL = "postgresql://ayomi:ayomi@db:5432/ayomi"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
